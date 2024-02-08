1eee881d08d11adc3e9868bf439d77e523c54cde
Jonatas Balmant <jonatas.balmant@loft.com.br>
2024-02-08 20:27:10 -0300

    fixing typo

--- solutions/array-jhow.js
+++ solutions/array-jhow.js
@@ -8,7 +8,7 @@ function somaDosElementos(array) {
   return soma;
 }
 
-console.log('**** EXERICIO 1')
+console.log('**** EXERCICIO 1')
 console.log(somaDosElementos([1, 2]))
 console.log(somaDosElementos([1, 2, 5]))
 console.log(somaDosElementos([1, 2, 5, 5]))
@@ -26,7 +26,7 @@ function maiorElementoDoArray(array) {
   return maiorElementoDoArray;
 }
 
-console.log('**** EXERICIO 2')
+console.log('**** EXERCICIO 2')
 console.log(maiorElementoDoArray([1, 2, 8, 3]));
 console.log(maiorElementoDoArray([241, 2332, 23428, 50000, 300]));
 
@@ -40,7 +40,7 @@ function mediaDosElementos(array) {
   return soma / array.length;
 }
 
-console.log('**** EXERICIO 3')
+console.log('**** EXERCICIO 3')
 console.log(mediaDosElementos([1, 2, 8, 5]));
 console.log(mediaDosElementos([3, 7]));
 console.log(mediaDosElementos([2, 1, 34, 42, 34, 3, 7]));
